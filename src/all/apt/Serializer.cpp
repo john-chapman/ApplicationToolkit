65f7580589a53f3b581e9cee2e13c62f9b98e187
John Chapman <contact@john-chapman.net>
2017-09-30 19:30:00 +0200

    Serializer: Template functions for internal impl

--- src/all/apt/Serializer.cpp
+++ src/all/apt/Serializer.cpp
@@ -16,117 +16,56 @@ void Serializer::setError(const char* _msg, ...)
 	va_end(args);
 }
 
-bool Serializer::value(vec2& _value_, const char* _name)
+template <typename T, uint Tlen>
+static bool ValueImpl(Serializer& _serializer_, T& _value_, const char* _name)
 {
-	uint len = 2;
-	if (beginArray(len, _name)) {
+	uint len = Tlen; \
+	if (_serializer_.beginArray(len, _name)) { \
 		bool ret = true;
-		for (int i = 0; i < (int)len; ++i) { 
-			ret &= value(_value_[i]);
+		if (len != Tlen) {
+			_serializer_.setError("Error serializing vec/mat %s: array length was %d, expected %d", _name ? _name : "", (int)len, (int)Tlen);
+			ret = false;
+		} else {
+			for (int i = 0; i < (int)Tlen; ++i) { 
+				ret &= _serializer_.value(_value_[i]);
+			}
 		}
-		endArray();
+		_serializer_.endArray();
 		return ret;
 	}
 	return false;
 }
+bool Serializer::value(vec2& _value_, const char* _name) { return ValueImpl<vec2,   2>(*this, _value_, _name); }
+bool Serializer::value(vec3& _value_, const char* _name) { return ValueImpl<vec3,   3>(*this, _value_, _name); }
+bool Serializer::value(vec4& _value_, const char* _name) { return ValueImpl<vec4,   4>(*this, _value_, _name); }
+bool Serializer::value(mat2& _value_, const char* _name) { return ValueImpl<mat2, 2*2>(*this, _value_, _name); }
+bool Serializer::value(mat3& _value_, const char* _name) { return ValueImpl<mat3, 3*3>(*this, _value_, _name); }
+bool Serializer::value(mat4& _value_, const char* _name) { return ValueImpl<mat4, 4*4>(*this, _value_, _name); }
 
-bool Serializer::value(vec3& _value_, const char* _name)
+template <typename T>
+static bool SerializeImpl(Serializer& _serializer_, T& _value_, const char* _name)
 {
-	uint len = 3;
-	if (beginArray(len, _name)) {
-		bool ret = true;
-		for (int i = 0; i < (int)len; ++i) { 
-			ret &= value(_value_[i]);
-		}
-		endArray();
-		return ret;
+	if (!_serializer_.value(_value_, _name)) {
+		APT_LOG_ERR(_serializer_.getError());
+		return false;
 	}
-	return false;
+	return true;
 }
-
-bool Serializer::value(vec4& _value_, const char* _name)
-{
-	uint len = 4;
-	if (beginArray(len, _name)) {
-		bool ret = true;
-		for (int i = 0; i < (int)len; ++i) { 
-			ret &= value(_value_[i]);
-		}
-		endArray();
-		return ret;
-	}
-	return false;
-}
-
-bool Serializer::value(mat2& _value_, const char* _name)
-{
-	uint len = 2*2;
-	if (beginArray(len, _name)) {
-		bool ret = true;
-		for (int i = 0; i < (int)len; ++i) { 
-			ret &= value(_value_[i]);
-		}
-		endArray();
-		return ret;
-	}
-	return false;
-}
-
-bool Serializer::value(mat3& _value_, const char* _name)
-{
-	uint len = 3*3;
-	if (beginArray(len, _name)) {
-		bool ret = true;
-		for (int i = 0; i < (int)len; ++i) { 
-			ret &= value(_value_[i]);
-		}
-		endArray();
-		return ret;
-	}
-	return false;
-}
-
-bool Serializer::value(mat4& _value_, const char* _name)
-{
-	uint len = 4*4;
-	if (beginArray(len, _name)) {
-		bool ret = true;
-		for (int i = 0; i < (int)len; ++i) { 
-			ret &= value(_value_[i]);
-		}
-		endArray();
-		return ret;
-	}
-	return false;
-}
-
-
-
-#define SerializeImpl(T) \
-	bool apt::Serialize(Serializer& _serializer_, T& _value_, const char* _name) \
-	{ \
-		if (!_serializer_.value(_value_, _name)) { \
-			APT_LOG_ERR(_serializer_.getError()); \
-			return false; \
-		} \
-		return true; \
-	}
-
-SerializeImpl(bool)
-SerializeImpl(sint8)
-SerializeImpl(uint8)
-SerializeImpl(sint16)
-SerializeImpl(uint16)
-SerializeImpl(sint32)
-SerializeImpl(uint32)
-SerializeImpl(sint64)
-SerializeImpl(uint64)
-SerializeImpl(float32)
-SerializeImpl(float64)
-SerializeImpl(StringBase)
-SerializeImpl(vec2)
-SerializeImpl(vec3)
-SerializeImpl(vec4)
-SerializeImpl(mat2)
-SerializeImpl(mat3)
-SerializeImpl(mat4)
+bool apt::Serialize(Serializer& _serializer_, bool&       _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, sint8&      _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, uint8&      _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, sint16&     _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, uint16&     _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, sint32&     _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, uint32&     _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, sint64&     _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, uint64&     _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, float32&    _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, float64&    _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, StringBase& _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, vec2&       _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, vec3&       _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, vec4&       _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, mat2&       _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, mat3&       _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
+bool apt::Serialize(Serializer& _serializer_, mat4&       _value_, const char* _name) { return SerializeImpl(_serializer_, _value_, _name); }
--- src/all/apt/Serializer.h
+++ src/all/apt/Serializer.h
@@ -11,6 +11,7 @@ namespace apt {
 
 ////////////////////////////////////////////////////////////////////////////////
 // Serializer
+// Base class for serialization handlers. 
 ////////////////////////////////////////////////////////////////////////////////
 class Serializer
 {
@@ -21,9 +22,10 @@ public:
 		Mode_Write
 	};
 
-	Mode        getMode() const                       { return m_mode; }
-	const char* getError() const                      { return (const char*)m_errStr; }
-	void        setError(const char* _msg, ...);
+	Mode         getMode() const                       { return m_mode;                }
+	void         setMode(Mode _mode)                   { m_mode = _mode;               }
+	const char*  getError() const                      { return (const char*)m_errStr; }
+	void         setError(const char* _msg, ...);
 
 	virtual bool beginObject(const char* _name = nullptr) = 0;
 	virtual void endObject() = 0;
@@ -44,18 +46,20 @@ public:
 	virtual bool value(float64&    _value_, const char* _name = nullptr) = 0;
 	virtual bool value(StringBase& _value_, const char* _name = nullptr) = 0;
 	
+	// vec* and mat* variants are implemented in terms of beginArray/endArray and value(float&).
+	bool         value(vec2& _value_, const char* _name = nullptr);
+	bool         value(vec3& _value_, const char* _name = nullptr);
+	bool         value(vec4& _value_, const char* _name = nullptr);
+	bool         value(mat2& _value_, const char* _name = nullptr);
+	bool         value(mat3& _value_, const char* _name = nullptr);
+	bool         value(mat4& _value_, const char* _name = nullptr);
+
+	// Helper to avoid explicit cast to StringBase&.
 	template <uint kCapacity>
-	bool value(String<kCapacity>& _value_, const char* _name = nullptr)
+	bool         value(String<kCapacity>& _value_, const char* _name = nullptr)
 	{
 		return value((StringBase&)_value_, _name); 
 	}
-
-	bool value(vec2& _value_, const char* _name = nullptr);
-	bool value(vec3& _value_, const char* _name = nullptr);
-	bool value(vec4& _value_, const char* _name = nullptr);
-	bool value(mat2& _value_, const char* _name = nullptr);
-	bool value(mat3& _value_, const char* _name = nullptr);
-	bool value(mat4& _value_, const char* _name = nullptr);
 
 protected:
 	Mode m_mode;
@@ -73,7 +77,7 @@ private:
 
 }; // class Serializer
 
-
+// Serialize* variants implicitly log an error if _serializer_.value() returns false.
 bool Serialize(Serializer& _serializer_, bool&        _value_, const char* _name = nullptr);
 bool Serialize(Serializer& _serializer_, sint8&       _value_, const char* _name = nullptr);
 bool Serialize(Serializer& _serializer_, uint8&       _value_, const char* _name = nullptr);
@@ -86,19 +90,17 @@ bool Serialize(Serializer& _serializer_, uint64&      _value_, const char* _name
 bool Serialize(Serializer& _serializer_, float32&     _value_, const char* _name = nullptr);
 bool Serialize(Serializer& _serializer_, float64&     _value_, const char* _name = nullptr);
 bool Serialize(Serializer& _serializer_, StringBase&  _value_, const char* _name = nullptr);
-
+bool Serialize(Serializer& _serializer_, vec2&        _value_, const char* _name = nullptr);
+bool Serialize(Serializer& _serializer_, vec3&        _value_, const char* _name = nullptr);
+bool Serialize(Serializer& _serializer_, vec4&        _value_, const char* _name = nullptr);
+bool Serialize(Serializer& _serializer_, mat2&        _value_, const char* _name = nullptr);
+bool Serialize(Serializer& _serializer_, mat3&        _value_, const char* _name = nullptr);
+bool Serialize(Serializer& _serializer_, mat4&        _value_, const char* _name = nullptr);
 template <uint kCapacity>
 bool Serialize(Serializer& _serializer_, String<kCapacity>& _value_, const char* _name = nullptr)
 {
 	return Serialize((StringBase&)_value_, _name); 
 }
-
-bool Serialize(Serializer& _serializer_, vec2& _value_, const char* _name = nullptr);
-bool Serialize(Serializer& _serializer_, vec3& _value_, const char* _name = nullptr);
-bool Serialize(Serializer& _serializer_, vec4& _value_, const char* _name = nullptr);
-bool Serialize(Serializer& _serializer_, mat2& _value_, const char* _name = nullptr);
-bool Serialize(Serializer& _serializer_, mat3& _value_, const char* _name = nullptr);
-bool Serialize(Serializer& _serializer_, mat4& _value_, const char* _name = nullptr);
 
 } // namespace apt
 
